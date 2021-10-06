04f9e47650b2bf7ca0140bf261f9bd5b955f17d1
Ruben <ruben_pages@hotmail.com.ar>
2021-10-05 21:09:30 -0300

    acercamiento a matelib.h y estructura

--- utils/src/matelib/matelib.h
+++ utils/src/matelib/matelib.h
@@ -1,4 +1,55 @@
 #ifndef _MATELIB_H_
 #define _MATELIB_H_
 
+
+#include <stdint.h>
+
+//-------------------Type Definitions----------------------/
+typedef struct mate_instance
+{
+     mate_inner_structure group_info; // deberia quedar el void*?
+} mate_instance;
+
+typedef struct{
+    uint_32 pid;
+    char* ip;
+    char* puerto;
+}mate_inner_structure;
+
+typedef char *mate_io_resource;
+
+typedef char *mate_sem_name;
+
+typedef int32_t mate_pointer;
+
+// TODO: Docstrings
+
+//------------------General Functions---------------------/
+int mate_init(mate_instance *lib_ref, char *config);
+
+int mate_close(mate_instance *lib_ref);
+
+//-----------------Semaphore Functions---------------------/
+int mate_sem_init(mate_instance *lib_ref, mate_sem_name sem, unsigned int value);
+
+int mate_sem_wait(mate_instance *lib_ref, mate_sem_name sem);
+
+int mate_sem_post(mate_instance *lib_ref, mate_sem_name sem);
+
+int mate_sem_destroy(mate_instance *lib_ref, mate_sem_name sem);
+
+//--------------------IO Functions------------------------/
+
+int mate_call_io(mate_instance *lib_ref, mate_io_resource io, void *msg);
+
+//--------------Memory Module Functions-------------------/
+
+mate_pointer mate_memalloc(mate_instance *lib_ref, int size);
+
+int mate_memfree(mate_instance *lib_ref, mate_pointer addr);
+
+int mate_memread(mate_instance *lib_ref, mate_pointer origin, void *dest, int size);
+
+int mate_memwrite(mate_instance *lib_ref, void *origin, mate_pointer dest, int size);
+
 #endif
