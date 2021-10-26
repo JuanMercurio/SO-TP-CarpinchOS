46d3131c7a95e8118d2cf375186047fb5b558790
lotuuu <nicolas@lotufo.com.ar>
2021-10-26 19:24:05 -0300

    Planteo inicial para funcion MEMALLOC

--- memoria/src/main.h
+++ memoria/src/main.h
@@ -3,6 +3,13 @@
 
 #include "tests/tests.h"
 
+
+typedef struct HeapMetadata {
+	uint32_t prevAlloc;
+	uint32_t nextAlloc;
+	uint8_t isFree;
+} HeapMetadata;
+
 /*
     @NAME: terminar_programa
     @DESC: Se encarga de liberar todas las estructuras y de hacer lo necesario para
